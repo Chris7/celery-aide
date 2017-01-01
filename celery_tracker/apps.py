763f053fb01ff2762f5e8259068b339561a87f90
Chris Mitchell <chris.mit7@gmail.com>
2016-12-31 20:52:57 -0500

    Fix spacing

--- celery_tracker/apps.py
+++ celery_tracker/apps.py
@@ -2,8 +2,8 @@ from django.apps import AppConfig
 
 
 class CeleryTrackerConfig(AppConfig):
-  name = 'celery_tracker'
-  verbose_name = 'Celery Tracker'
+    name = 'celery_tracker'
+    verbose_name = 'Celery Tracker'
 
-  def ready(self):
-      pass
+    def ready(self):
+        pass
